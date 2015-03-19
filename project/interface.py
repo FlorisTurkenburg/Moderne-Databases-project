5be265876f3706be47172630bf9f619a3774cfd6
Floris <sk8_floris@hotmail.com>
2015-03-19 15:49:12 +0100

    webInterface kan inserten met curl

--- project/interface.py
+++ project/interface.py
@@ -1,5 +1,5 @@
 from tornado.ioloop import IOLoop
-from tornado.web import RequestHandler, Application, url
+from tornado.web import RequestHandler, Application, url, RedirectHandler
 import btree
 
 
@@ -17,36 +17,15 @@ class DocumentsHandler(RequestHandler):
         self.db = db
 
     def get(self):
-        self.write("All documents:</br>")
-        for doc in self.db._get_documents():
-            self.write('<a href="%s">link to document: %s</a></br>' % 
-                (self.reverse_url("document", doc), doc))
+        # self.write("All documents:</br>")
+        
+        # for doc in self.db._get_documents():
+        #     self.write('<a href="%s">link to document: %s</a></br>' % 
+        #         (self.reverse_url("document", doc), doc))
 
-        self.write('<br><a href="/insertDoc/">Click here to insert a document.</a>')
+        # self.write('<br><a href="/insertDoc/">Click here to insert a document.</a>')
+        self.render("doc_list.html", title="All Documents", items=self.db._get_documents())
 
-
-class DocumentHandler(RequestHandler):
-    def initialize(self, db):
-        self.db = db
-
-    def get(self, doc_key):
-        self.write("Content of document " + str(doc_key) + " :\n" + str(self.db[doc_key]))
-
-
-class InsertDocHandler(RequestHandler):
-    def initialize(self, db):
-        self.db = db
-
-    def get(self):
-        self.write('<html><body><form action="/insertDoc/" method="POST">'
-                    'Key:<br>'
-                    '<input type="text" name="docKey"><br>'
-                    'Document content:<br>'
-                    '<input type="text" name="docContent"><br>'
-                    '<input type="submit" value="Insert Document">'
-                    '</form></body></html>')
-
-    
     def post(self):
         self.set_header("Content-Type", "text/plain")
         key = self.get_body_argument("docKey")
@@ -54,7 +33,33 @@ class InsertDocHandler(RequestHandler):
         self.db[key] = value
         self.db._commit("data")
 
-        self.write("You inserted key=" + repr(key) + " with value=" + repr(value) + " into the database." )
+        message = "You inserted key=" + repr(key) + " with value=" + repr(value) + " into the database."
+        self.write('alert("%s")' % message)
+
+
+
+class DocumentHandler(RequestHandler):
+    def initialize(self, db):
+        self.db = db
+
+    def get(self, doc_key):
+        self.write("Content of document " + str(doc_key) + " :<br>" + str(self.db[doc_key]))
+
+
+class InsertDocHandler(RequestHandler):
+    def initialize(self, db):
+        self.db = db
+
+    def get(self):
+        self.write('<html><body><form action="/documents/" method="POST">'
+                    'Key:<br>'
+                    '<input type="text" name="docKey" required><br>'
+                    'Document content:<br>'
+                    '<input type="text" name="docContent" required><br>'
+                    '<input type="submit" value="Insert Document">'
+                    '</form></body></html>')
+
+    
 
 
 
@@ -62,8 +67,9 @@ def make_app():
     tree = btree.start_up(4)
     return Application([
         url(r"/", MainHandler),
+        url(r"/documents", RedirectHandler, dict(url=r"/documents/") ),
         url(r"/documents/", DocumentsHandler, dict(db=tree), name="documents"),
-        url(r"/documents/([a-z0-9]+)", DocumentHandler, dict(db=tree), name="document"),
+        url(r"/document/([a-zA-Z0-9_]+)", DocumentHandler, dict(db=tree), name="document"),
         url(r"/insertDoc/", InsertDocHandler, dict(db=tree), name="insertdocument")
     ])
 
